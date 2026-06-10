c7e9b6f92b675c604917344851318e05f729b318
Shivathmika20 <yelurishivathmika@gmail.com>
2026-06-10 12:50:00 +0530

    feat: implemented ai process handler to handle the tasks

--- src/controllers/aiController.ts
+++ src/controllers/aiController.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express";
-const processHandler=(req:Request,res:Response)=>{
-    console.log(req.body);
+
+export const processHandler=async (req:Request,res:Response)=>{
+    const {transcribedText,prompt}=req.body;
+
+    if(!transcribedText){
+        return res.status(400).json({
+            error:"Text is needed"
+        })
+    }
+
+    //todo call ai service to get structerd text
+    return res.status(200).json({
+        structure:"structured text here"
+    })
 }
 
-export default processHandler;
