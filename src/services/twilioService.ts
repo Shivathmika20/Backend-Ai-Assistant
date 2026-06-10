2d895676b6c58b4642b8f1eec31fad0d6e82841e
Shivathmika20 <yelurishivathmika@gmail.com>
2026-06-10 20:01:25 +0530

    feat: add SMS sending functionality using Twilio service

---
+++ src/services/twilioService.ts
@@ -0,0 +1,20 @@
+import { Twilio } from "twilio";
+import {config} from "../config/var"
+const client=new Twilio(config.TWILIO_ACCOUNT_SID,config.TWILIO_AUTH_TOKEN)
+
+export const sendSMS=async(docLink:string)=>{
+   try{
+    const message=await client.messages.create({
+        body: `Your note is saved! 📝 View here → https://docs.google.com/document/d/${docLink}`,
+        from:config.TWILIO_PHONE_NUMBER,
+        to:config.MY_PHONE_NUMBER
+    });
+
+    console.log(`Success! Message Sent. SID: ${message.sid}`)
+    return message.sid; // confirms SMS was sent
+   }
+   catch(e){
+    console.error(`Failed to send message: ${e}`);
+   }
+
+}
