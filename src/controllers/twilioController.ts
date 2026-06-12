d3e5d680b8260c1c001b1d660358639fd93b0841
Shivathmika20 <yelurishivathmika@gmail.com>
2026-06-12 15:00:55 +0530

    feat: enhance Twilio callback handler to transcribe audio and save notes to Google Drive

--- src/controllers/twilioController.ts
+++ src/controllers/twilioController.ts
@@ -1,34 +1,49 @@
 import { Request, Response } from "express";
+import {sendSMS} from "../services/twilioService";
+import { transcribeAudio } from "../services/assemblyService";
+import { saveToDoc } from "../services/gdriveService";
+import { processText } from "../services/aiServcie";
 
+export const callbackHandler= async (req:Request,res:Response)=>{ 
+    
+    try{
+        const {RecordingUrl,From}=req.body
 
-export const callbackHandler= async (req:Request,res:Response)=>{
-    const {RecordingUrl,From}=req.body
-
-   if(!RecordingUrl || !From){
-    return res.status(400).json({
-        error:" Audio URl and Calling number is required"
-    })
-   }
-
-   //TODO: Pass the Audio URL to the Assembly API
-   console.log(RecordingUrl);
-   console.log(From);
-
-   return res.status(200).json({
-    message:"Audio URL received"
-   })
-}
-
-export const notifyHandler=async(req:Request,res:Response)=>{
-    const {docLink} = req.body;
-    if(!docLink){
-        return res.status(400).json({
-            error:"Document Link is missing"
+        if(!RecordingUrl || !From){
+         return res.status(400).json({
+             error:" Audio URl and Calling number is required"
+         })
+        }
+     
+        const title = `Voice Note - ${new Date().toLocaleDateString('en-IN', { 
+         day: '2-digit', month: 'short', year: 'numeric' 
+         })}`;
+        
+        const transcript=await transcribeAudio(RecordingUrl);
+        const structuredContent = await processText(transcript!);
+        const docLink = await saveToDoc(title, structuredContent!);
+        await sendSMS(docLink);
+     
+        return res.status(200).json({
+         message: "Note saved successfully!" 
         })
     }
-     // TODO: send SMS via twilioService
-     return res.status(200).json({
-        message:"SMS sent successfully"
-     })
+    catch(e:any){
+        console.log(e.message)
+    }
+    
 }
+
+//handling initail call
+export const callAnswer=(req:Request,res:Response)=>{
+    res.type('text/xml');
+    res.send(`
+        <Response>
+            <Say>Hello! Please leave your note after the beep.</Say>
+            <Record maxLength="120" action="/api/twilio/callback" />
+        </Response>
+    `);
+}
+
+
 
