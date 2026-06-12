6d962746f8d5ac42e03e93871dd0dc5586eccb62
Shivathmika20 <yelurishivathmika@gmail.com>
2026-06-12 14:58:58 +0530

    feat: implement OAuth2 for Google API authentication and drive implementation

--- src/services/gdriveService.ts
+++ src/services/gdriveService.ts
@@ -1,47 +1,45 @@
 import { google } from "googleapis";
 import {config} from "../config/var"
 
-const auth =new google.auth.JWT({
-    email:config.GOOGLE_CLIENT_EMAIL,
-    key:config.GOOGLE_PRIVATE_KEY,
-    scopes:[
-        "https://www.googleapis.com/auth/drive",
-        "https://www.googleapis.com/auth/documents"
-    ]
-})
+const oauth2Client=new google.auth.OAuth2(
+    config.GOOGLE_CLIENT_ID,
+    config.GOOGLE_CLIENT_SECRET,
+)
 
-export const saveToDoc=async(title:string,content:string)=>{
-    const docs=google.docs({version:"v1",auth}) //get access to doc and drive
-    const drive=google.drive({version:"v3",auth})
+oauth2Client.setCredentials({
+    refresh_token:config.GOOGLE_REFRESH_TOKEN
+});
 
+export const saveToDoc = async (title: string, content: string) => {
+    const docs = google.docs({ version: "v1", auth: oauth2Client });
+    const drive = google.drive({ version: "v3", auth: oauth2Client });
 
-    const doc=await docs.documents.create({ //create an empty doc with tittle it return id
-        requestBody:{
-            title:title
-        }
-    })
+    const doc = await docs.documents.create({
+        requestBody: { title: title }
+    });
 
-    const documentId=doc.data.documentId //store the id
+    const documentId = doc.data.documentId;
+    console.log("Doc created, id:", documentId);
 
-    await docs.documents.batchUpdate({ //write into that doc
-        documentId:documentId!,
-        requestBody:{
-            requests:[{
-                insertText:{
-                    location:{index:1},
-                    text:content
+    await docs.documents.batchUpdate({
+        documentId: documentId!,
+        requestBody: {
+            requests: [{
+                insertText: {
+                    location: { index: 1 },
+                    text: content
                 }
             }]
         }
-
     });
 
-    await drive.files.update({ //store taht doc in drive
-        fileId:documentId!,
-        addParents:config.GOOGLE_DRIVE_FOLDER_ID,
-        requestBody:{}
-    })
+    // Move doc to your specific folder
+    await drive.files.update({
+        fileId: documentId!,
+        addParents: config.GOOGLE_DRIVE_FOLDER_ID,
+        removeParents: 'root',
+        requestBody: {}
+    });
 
-    return `https://docs.google.com/document/d/${documentId}`;  //return drive link to notify
-    
+    return `https://docs.google.com/document/d/${documentId}`;
 }
